ab09de72672221f8d82c2b648f06f8ee5c07779d
vedamitragurram <vedamitragurram@gmail.com>
2025-08-09 19:31:19 +0530

    week2 submission

---
+++ week2/factorial.c
@@ -0,0 +1,10 @@
+#include "unique.h"
+#include <stdio.h>
+
+static unsigned long long fact(unsigned n){ return (n<2)?1ULL:n*fact(n-1); }
+int main(void){
+	uniq_print_header("factorial");
+	unsigned n = 12;
+	printf("n=%u, n!=%llu\n", n, fact(n));
+	return 0;
+}
